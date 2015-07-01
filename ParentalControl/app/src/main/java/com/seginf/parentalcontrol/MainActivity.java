4755bdf77911f74ce9184d8898cf0b9906ed8811
Santiago <santiago.alvarez@mercadolibre.com>
2015-07-01 18:33:31 -0300

    parental control initial UI

--- ParentalControl/app/src/main/java/com/seginf/parentalcontrol/MainActivity.java
+++ ParentalControl/app/src/main/java/com/seginf/parentalcontrol/MainActivity.java
@@ -2,6 +2,8 @@ package com.seginf.parentalcontrol;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,4 +14,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void createProfile(View v) {
+        Toast.makeText(this, "Created Profile", Toast.LENGTH_LONG).show();
+    }
+
+    public void keepCallLog(View v) {
+        Toast.makeText(this, "Call log will be saved", Toast.LENGTH_LONG).show();
+    }
+
+    public void createLocationMap(View v) {
+        Toast.makeText(this, "Tracking kids location", Toast.LENGTH_LONG).show();
+    }
 }
