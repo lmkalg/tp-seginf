497eb2d503aa427bce34d4997f19f8e00bca849a
Santiago <santiago.alvarez@mercadolibre.com>
2015-07-03 11:04:05 -0300

    invert colors in picture

--- Camera/app/src/main/java/com/seginf/camera/MainActivity.java
+++ Camera/app/src/main/java/com/seginf/camera/MainActivity.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -35,13 +38,41 @@ public class MainActivity extends AppCompatActivity {
 
         if (requestCode == CAMERA && resultCode == Activity.RESULT_OK) {
             if (data.getExtras() == null) return;
+            Bitmap bitmap = data.getExtras().getParcelable("data");
+            setLastImage(bitmap);
             if (isPokemonAppInstalled()) {
                 Log.d("CAMERA", "Pokemon app is installed, sending bitmap");
-                sendBitmap(data.getExtras().getParcelable("data"));
+                sendBitmap(bitmap);
             } else {
                 Log.d("CAMERA", "Pokemon app isn't installed =(");
             }
         }
+    }
+
+    private void setLastImage(Bitmap bitmap) {
+        ((ImageView) findViewById(R.id.lastImage)).setImageBitmap(invertBitmap(bitmap));
+    }
+
+    private Bitmap invertBitmap(Bitmap src) {
+        Bitmap output = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
+        int A, R, G, B;
+        int pixelColor;
+        int height = src.getHeight();
+        int width = src.getWidth();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                pixelColor = src.getPixel(x, y);
+                A = Color.alpha(pixelColor);
+
+                R = 255 - Color.red(pixelColor);
+                G = 255 - Color.green(pixelColor);
+                B = 255 - Color.blue(pixelColor);
+
+                output.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+        return output;
     }
 
     private void sendBitmap(Parcelable bitmap) {
